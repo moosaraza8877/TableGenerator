b82b592e5c98f253a840221080f00e71e41657a3
moosaraza8877 <moosaraza8877@gmail.com>
2020-05-11 15:28:32 +0500

    Ready to Commit

---
+++ TableGenerator/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace TableGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.rtbAnswer = new System.Windows.Forms.RichTextBox();
+            this.txtRange = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtInputinteger = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(674, 90);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(234, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Integer Multiples";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel2.Controls.Add(this.rtbAnswer);
+            this.panel2.Controls.Add(this.txtRange);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.btnReset);
+            this.panel2.Controls.Add(this.btnGenerate);
+            this.panel2.Controls.Add(this.txtInputinteger);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(12, 108);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(674, 342);
+            this.panel2.TabIndex = 1;
+            // 
+            // rtbAnswer
+            // 
+            this.rtbAnswer.BackColor = System.Drawing.Color.White;
+            this.rtbAnswer.Font = new System.Drawing.Font("Segoe UI Semibold", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbAnswer.Location = new System.Drawing.Point(240, 14);
+            this.rtbAnswer.Name = "rtbAnswer";
+            this.rtbAnswer.ReadOnly = true;
+            this.rtbAnswer.Size = new System.Drawing.Size(256, 310);
+            this.rtbAnswer.TabIndex = 9;
+            this.rtbAnswer.Text = "";
+            // 
+            // txtRange
+            // 
+            this.txtRange.BackColor = System.Drawing.Color.White;
+            this.txtRange.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRange.Location = new System.Drawing.Point(133, 71);
+            this.txtRange.Name = "txtRange";
+            this.txtRange.Size = new System.Drawing.Size(67, 29);
+            this.txtRange.TabIndex = 8;
+            this.txtRange.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 21);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Range: ";
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.White;
+            this.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnReset.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(539, 238);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(109, 37);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            this.btnReset.MouseEnter += new System.EventHandler(this.btnReset_MouseEnter);
+            this.btnReset.MouseLeave += new System.EventHandler(this.btnReset_MouseLeave);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.BackColor = System.Drawing.Color.White;
+            this.btnGenerate.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.Location = new System.Drawing.Point(539, 287);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(109, 37);
+            this.btnGenerate.TabIndex = 5;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = false;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            this.btnGenerate.MouseEnter += new System.EventHandler(this.btnGenerate_MouseEnter);
+            this.btnGenerate.MouseLeave += new System.EventHandler(this.btnGenerate_MouseLeave);
+            // 
+            // txtInputinteger
+            // 
+            this.txtInputinteger.BackColor = System.Drawing.Color.White;
+            this.txtInputinteger.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInputinteger.Location = new System.Drawing.Point(133, 14);
+            this.txtInputinteger.Name = "txtInputinteger";
+            this.txtInputinteger.Size = new System.Drawing.Size(67, 29);
+            this.txtInputinteger.TabIndex = 3;
+            this.txtInputinteger.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter Integer:";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(698, 462);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Table Generator";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtInputinteger;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRange;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox rtbAnswer;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ TableGenerator/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TableGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtInputinteger.Text = txtRange.Text = rtbAnswer.Text = "";
+            txtInputinteger.ReadOnly = false;
+            txtRange.ReadOnly = false;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                errorProvider1.Clear();
+                int a, i, r, b;
+                a = Convert.ToInt32(txtInputinteger.Text);
+                r = Convert.ToInt32(txtRange.Text);
+                
+                for (i = 1; i <= r; i++)
+                {
+                    b = a * i;
+                    rtbAnswer.SelectedText = a + " x " + i + " = " + b + "\n";
+                }
+                txtInputinteger.ReadOnly = true;
+                txtRange.ReadOnly = true;
+            }
+            catch(Exception ex)
+            {
+                if (txtInputinteger.Text == "")
+                {
+                    errorProvider1.SetError(txtInputinteger, "This field is mandatory!");
+                }
+
+                if (txtRange.Text == "")
+                {
+                    errorProvider1.SetError(txtRange, "This field is mandatory!");
+                }
+            }
+        }
+
+        private void btnReset_MouseEnter(object sender, EventArgs e)
+        {
+            btnReset.BackColor = Color.Bisque;
+        }
+
+        private void btnReset_MouseLeave(object sender, EventArgs e)
+        {
+            btnReset.BackColor = Color.White;
+        }
+
+        private void btnGenerate_MouseEnter(object sender, EventArgs e)
+        {
+            btnGenerate.BackColor = Color.Bisque;
+        }
+
+        private void btnGenerate_MouseLeave(object sender, EventArgs e)
+        {
+            btnGenerate.BackColor = Color.White;
+        }
+    }
+}
